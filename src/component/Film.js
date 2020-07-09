4177e7e6005e0b5ba1716d7fc1c0a3275cccb76c
hbouachir <bouachirhamza@gmail.com>
2020-07-09 17:57:30 +0100

    V0.8

--- src/component/Film.js
+++ src/component/Film.js
@@ -1,43 +1,52 @@
-import React from 'react'
+import React , {useState} from 'react'
 import '../App.css';
 import Card from 'react-bootstrap/Card'
-import ReactStars from "react-rating-stars-component"
+import StarRatingComponent from 'react-star-rating-component';
 import '../App.css';
+import ReactStars from "react-rating-stars-component";
+
 
 
 function Film({filmList,setKeyword,keyword,setFilmList,newFilms,setNewFilms}) {
- 
+  const [rate,setRate]=useState(1)
+  const ratingChanged = (newRating) => {
+    setRate(newRating);
+  };
+
 
     return (
+     
       <div className="film">
         
  <input className="gate"  type="text" placeholder="entrer nom film" onChange={(e)=>setKeyword(e.target.value)} />
  
  
-      <button onClick={()=>setNewFilms(filmList.filter(el=>el.Name.includes(keyword))) }
+      <button onClick={()=>setNewFilms(filmList.filter(el=>el.Name.includes(keyword)&&el.Rate>=rate)) }
               >search</button>
+              <ReactStars
+    count={5}
+    value={rate}
+    onChange={ratingChanged}
+    size={24}
+    color2={"#ffd700"}
+  />
         
 <br/>
       <div className="flex">
         { newFilms.map(el=>(
           
                <div className="sub">
-               <Card style={{ width: '18rem'}}>
+               <Card style={{ width: '20rem', height:'100%'}}>
                 <Card.Img className='img' variant="top" src={el.URL} />
                 <Card.Body>
                   <Card.Title>{el.Name}</Card.Title>
                   <Card.Text>
-                  <ReactStars
-                    count={5}
+                  <StarRatingComponent 
+                    name="rate2" 
+                    starCount={5}
                     value={el.Rate}
-                    onChange={e=>{console.log(e)}}
-                  size={24}
-                 half={true}
-               emptyIcon={<i className="far fa-star"></i>}
-              halfIcon={<i className="fa fa-star-half-alt"></i>}
-            fullIcon={<i className="fa fa-star"></i>}
-            color2={"#ffd700"}
-  />,
+                  />
+                  
                     
                   </Card.Text>
                 </Card.Body>
